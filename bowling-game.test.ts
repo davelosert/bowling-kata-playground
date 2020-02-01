31deb6faa341c23dd64079d4887449ddb0accf98
David Losert <david.losert@gmx.de>
2020-02-01 18:34:37 +0100

    Refactored test code to reflect a full game

--- bowling-game.test.ts
+++ bowling-game.test.ts
@@ -1,21 +1,20 @@
 import {createGame} from './bowling-game';
 
 describe('Bowling Game', () => {
-    it('score is 0 when rolling only 0 in a frame', () => {
+    it('score is 0 when rolling only 0', () => {
         const game = createGame();
-        game.roll(0);
-        game.roll(0);
+        for(let i = 0; i < 20; i++) {
+            game.roll(0);
+        }
         expect(game.score()).toBe(0);
     });
 
-    it('score is 1 when rolling only 1 in a frame', () => {
+    it('score is 20 when rolling only 1s', () => {
         const game = createGame();
-        game.roll(0);
-        game.roll(1);
-        expect(game.score()).toBe(1);
+        for(let i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+        expect(game.score()).toBe(20);
     });
 
-    it('test', () => {
-        
-    });
 });
