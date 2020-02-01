37a421d0634aed4ac58a0a0a45468d731fad4d30
David Losert <david.losert@gmx.de>
2020-02-01 18:32:02 +0100

    Added first two testcases

--- bowling-game.test.ts
+++ bowling-game.test.ts
@@ -1,7 +1,21 @@
 import {createGame} from './bowling-game';
 
 describe('Bowling Game', () => {
-    it('works', () => {
+    it('score is 0 when rolling only 0 in a frame', () => {
         const game = createGame();
+        game.roll(0);
+        game.roll(0);
+        expect(game.score()).toBe(0);
+    });
+
+    it('score is 1 when rolling only 1 in a frame', () => {
+        const game = createGame();
+        game.roll(0);
+        game.roll(1);
+        expect(game.score()).toBe(1);
+    });
+
+    it('test', () => {
+        
     });
 });
--- bowling-game.ts
+++ bowling-game.ts
@@ -1,2 +1,12 @@
 export const createGame = () => {
+    let scored = false;
+    let score = 0;
+    return {
+        roll: (pins: number) => {
+            score += pins;
+        },
+        score: (): number => {
+            return score;
+        }
+    }
 }
