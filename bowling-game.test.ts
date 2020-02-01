ea0e61abebbdc626b11ac456643e087ec5b298d8
David Losert <david.losert@gmx.de>
2020-02-01 18:40:21 +0100

    Added test for spare calculation

--- bowling-game.test.ts
+++ bowling-game.test.ts
@@ -1,20 +1,34 @@
 import {createGame} from './bowling-game';
 
 describe('Bowling Game', () => {
-    it('score is 0 when rolling only 0', () => {
-        const game = createGame();
-        for(let i = 0; i < 20; i++) {
-            game.roll(0);
+    let game;
+
+    const rollMany = (timesRolled: number, score: number) => {
+        for(let i = 0; i < timesRolled; i++) {
+            game.roll(score);
         }
+    }
+    beforeEach(() => {
+        game = createGame();
+    });
+
+    it('score is 0 when rolling only 0', () => {
+        rollMany(20, 0);
         expect(game.score()).toBe(0);
     });
 
     it('score is 20 when rolling only 1s', () => {
-        const game = createGame();
-        for(let i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        rollMany(20, 1);
         expect(game.score()).toBe(20);
     });
 
+    it('score is 13 when rolling a spare followed by 3 pins', () => {
+        game.roll(5);
+        game.roll(5);
+        game.roll(3);
+        rollMany(17, 0);
+        expect(game.score()).toBe(16);
+
+    });
+    
 });
