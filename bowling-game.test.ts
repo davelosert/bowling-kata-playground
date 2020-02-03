ba09b147768492a8e9dbb12bc6091916b9eb284b
David Losert <david.losert@gmx.de>
2020-02-03 11:09:24 +0100

    Implemented a basic FP approach with the kata

--- bowling-game.test.ts
+++ bowling-game.test.ts
@@ -1,34 +1,68 @@
-import {createGame} from './bowling-game';
-
+import { createInitialGameState, rollPins, getScore as calculateScore } from './bowling-game';
 describe('Bowling Game', () => {
-    let game;
-
-    const rollMany = (timesRolled: number, score: number) => {
-        for(let i = 0; i < timesRolled; i++) {
-            game.roll(score);
-        }
-    }
+    let gameState;
     beforeEach(() => {
-        game = createGame();
+        gameState = createInitialGameState();
     });
 
-    it('score is 0 when rolling only 0', () => {
-        rollMany(20, 0);
-        expect(game.score()).toBe(0);
+    it('scores 0 when rolling only 0s', () => {
+        rollMany(gameState, 20, 0);
+
+        expect(calculateScore(gameState)).toBe(0);
     });
 
-    it('score is 20 when rolling only 1s', () => {
-        rollMany(20, 1);
-        expect(game.score()).toBe(20);
+    it('scores 20 when rolling only 1s', () => {
+        rollMany(gameState, 20, 1);
+
+        expect(calculateScore(gameState)).toBe(20);
     });
 
-    it('score is 13 when rolling a spare followed by 3 pins', () => {
-        game.roll(5);
-        game.roll(5);
-        game.roll(3);
-        rollMany(17, 0);
-        expect(game.score()).toBe(16);
+    it('adds score of next roll on spare', () => {
+        rollPins(gameState, 5);
+        rollPins(gameState, 5);
+        rollPins(gameState, 3);
+        rollMany(gameState, 17, 0);
 
+        expect(calculateScore(gameState)).toBe(16);
     });
-    
+
+    it('adds score of next frame on strike', () => {
+        rollPins(gameState, 10);
+        rollPins(gameState, 3);
+        rollPins(gameState, 4);
+        rollMany(gameState, 16, 0);
+
+        expect(calculateScore(gameState)).toBe(24);
+    });
+
+    it('does not count a spare if score is 10 in two different frames', () => {
+        rollPins(gameState, 0);
+        rollPins(gameState, 5);
+        rollPins(gameState, 5);
+        rollPins(gameState, 3);
+
+        expect(calculateScore(gameState)).toBe(13);
+    });
+
+    it('scores 300 on perfect game', () => {
+        rollMany(gameState, 12, 10);
+
+        expect(calculateScore(gameState)).toBe(300);
+    });
+
+   it('counts strikes in normal games', () => {
+       rollMany(gameState, 10, 0);
+       rollPins(gameState, 10);
+       rollPins(gameState, 3);
+       rollPins(gameState, 4);
+       rollMany(gameState, 6, 0);
+       expect(calculateScore(gameState)).toBe(24);
+
+       
+   }); 
 });
+
+function rollMany(gameState: any, rollCount: number, rollScore: number) {
+    for (let i = 0; i < rollCount; i++)
+        rollPins(gameState, rollScore);
+}
--- bowling-game.ts
+++ bowling-game.ts
@@ -1,21 +1,25 @@
-export const createGame = () => {
-    let rolls = [];
-    return {
-        roll: (pins: number) => {
-            rolls = [...rolls, pins];
-        },
-        score: (): number => {
-            let rollCounter = 0;
-            let sum = 0;
-            for(let frame = 0; frame < 10; frame++){
-                const currentScore = rolls[rollCounter] + rolls[rollCounter + 1];
-                if(currentScore === 10) {
-                    sum += rolls[rollCounter + 2];
-                }
-                sum += currentScore;
-                rollCounter += 2;
-            }
-            return sum;
+export const createInitialGameState = () => ({
+    rolledPins: []
+});
+
+export const rollPins = (gameState, knockedPins: number) => {
+    gameState.rolledPins = [...gameState.rolledPins, knockedPins];
+};
+
+export const getScore = (gameState): number => {
+    return gameState.rolledPins.reduce((currentScore, rolledPins, index) => {
+        if (isNotBonusFrame(gameState, index) && isStrike(rolledPins)) {
+            currentScore += gameState.rolledPins[index + 1] + gameState.rolledPins[index + 2];
+        } else if (isFirstRollOfFrame(index) && isSpare(rolledPins, gameState, index)) {
+            currentScore += gameState.rolledPins[index + 2];
         }
-    }
-}
+        currentScore += rolledPins;
+        return currentScore;
+    }, 0);
+};
+
+const isNotBonusFrame = (gameState: any, index: number) => gameState.rolledPins.length > index + 3;
+const isStrike = (rolledPins: any) => rolledPins === 10;
+const isFirstRollOfFrame = (index: number) => index % 2 === 0;
+const isSpare = (rolledPins: any, gameState: any, index: number) => rolledPins + gameState.rolledPins[index + 1] === 10;
+
