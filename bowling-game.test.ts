78c447dfeb084cc4a00b9c28cff5cc268dabd3ad
David Losert <david.losert@gmx.de>
2020-02-04 15:26:07 +0100

    Deleted all code to have a blank starting point on master

--- bowling-game.test.ts
+++ bowling-game.test.ts
@@ -1,34 +1,3 @@
-import {createGame} from './bowling-game';
-
 describe('Bowling Game', () => {
-    let game;
-
-    const rollMany = (timesRolled: number, score: number) => {
-        for(let i = 0; i < timesRolled; i++) {
-            game.roll(score);
-        }
-    }
-    beforeEach(() => {
-        game = createGame();
-    });
-
-    it('score is 0 when rolling only 0', () => {
-        rollMany(20, 0);
-        expect(game.score()).toBe(0);
-    });
-
-    it('score is 20 when rolling only 1s', () => {
-        rollMany(20, 1);
-        expect(game.score()).toBe(20);
-    });
-
-    it('score is 13 when rolling a spare followed by 3 pins', () => {
-        game.roll(5);
-        game.roll(5);
-        game.roll(3);
-        rollMany(17, 0);
-        expect(game.score()).toBe(16);
-
-    });
     
 });
